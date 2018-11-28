582620fe2387e83fa0f47750d6b88ec90f61f0f3
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-11-28 08:46:03 -0500

    populated js file

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+// listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+// save bookmark
+function saveBookmark (e) {
+    var siteName = document.getElementById('siteName');
+
+
+    // Prevents form submission
+    e.preventDefault();
+}
