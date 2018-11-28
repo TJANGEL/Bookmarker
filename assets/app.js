58d111a8237122a90a292e967539ede3926a3bf1
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-11-28 09:03:28 -0500

    populated js file

--- assets/app.js
+++ assets/app.js
@@ -3,9 +3,37 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // save bookmark
 function saveBookmark (e) {
-    var siteName = document.getElementById('siteName');
+    var siteName = document.getElementById('siteName').value;
+    var siteURL = document.getElementById('siteURL').value;
 
+    // create bookmark object
+    var bookmark = {
+        name: siteName,
+        url: siteURL
+    }
+    console.log(bookmark);
+
+    if (localStorage.getItem('bookmarks') === null) {
+        var bookmarks = [];
+
+        bookmarks.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        // get bookmarks from local storage / turns string into JSON
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+        // add bookmarks to array
+        bookmarks.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
 
     // Prevents form submission
     e.preventDefault();
+}
+
+// fetch bookmarks
+function fetchBookmarks () {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 }
